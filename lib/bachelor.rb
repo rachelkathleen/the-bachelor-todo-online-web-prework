aa9b3c6c9c18a86723fd311c47a4020346c5aed8
Rachel Hawa <rachel.hawa@gmail.com>
2018-11-14 05:10:22 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -10,11 +10,11 @@ end
 def get_contestant_name(data, occupation)
   data.each do |season, contestants|
     contestants.each do |contestant_hash|
-    if contestant_hash["occupation"] == occupation
-    return contestant_hash["name"]
+      if contestant_hash["occupation"] == occupation
+        return contestant_hash["name"]
+      end
+    end
   end
-end
-end
 end
 
 def count_contestants_by_hometown(data, hometown)

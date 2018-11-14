defaf3d0e2da85c11636f37f4dfa010d737f5d36
Rachel Hawa <rachel.hawa@gmail.com>
2018-11-14 05:01:23 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,7 +1,7 @@
 require 'pry'
 def get_first_name_of_season_winner(data, season)
   data.each do |season, contestant_hash|
-    when contestant_hash["status"] == "winner"
+    if contestant_hash["status"] == "winner"
       return contestant_hash["name"]
   end
 end
